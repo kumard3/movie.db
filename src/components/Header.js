32027884859bba9ed88580544edd45ed7fd0792d
kumard3 <kumardeepanshu157200@gmail.com>
2021-07-02 02:12:25 +0530

    banner__half__done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Nav from './Nav'
+import Banner from './Banner'
+import '../styles/header.scss'
 const Header = () => {
     return (
-        <div>
+        <div className="header" >
+        <Nav/>
+        <Banner/>
             
         </div>
     )
