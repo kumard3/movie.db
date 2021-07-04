95c547b7ad597a2f4dde8b039921c3d8d5546cc3
kumard3 <kumardeepanshu157200@gmail.com>
2021-07-04 22:49:11 +0530

    update

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import '../styles/body.scss'
-import Card from './Card';
-import requests from "../api/data"
+import React from "react";
+import "../styles/body.scss";
+
+import requests from "../api/data";
 import black from "../assets/Rectangle.png";
+import Card from "./Card";
 
 const Body = () => {
-    return (
-        <div className="body" >
-        <img className="body__bottom" src={black} alt={black} />
-        <div className="body__cardWrapper" > 
-        <Card fetchUrl={requests.fetchPage1} />
-        <Card fetchUrl={requests.fetchPage2} />
-        <Card fetchUrl={requests.fetchPage3} />
-        <Card fetchUrl={requests.fetchPage4} />
-        <Card fetchUrl={requests.fetchPage5} />
-        <Card fetchUrl={requests.fetchPage6} />
-        <Card fetchUrl={requests.fetchPage7} />
-        </div>
-        </div>
-    )
-}
+  return (
+    <div className="body">
+      <img className="body__bottom" src={black} alt={black} />
+      <div className="body__cardWrapper">
+        <Card className="body__card" fetchUrl={requests.fetchPage1} />
+        <Card className="body__card" fetchUrl={requests.fetchPage2} />
+        <Card className="body__card" fetchUrl={requests.fetchPage3} />
+        <Card className="body__card" fetchUrl={requests.fetchPage4} />
+        <Card className="body__card" fetchUrl={requests.fetchPage5} />
+        <Card className="body__card" fetchUrl={requests.fetchPage6} />
+        <Card className="body__card" fetchUrl={requests.fetchPage7} />
+      </div>
+    </div>
+  );
+};
 
-export default Body
+export default Body;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,43 +1,52 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import '../styles/card.scss'
+import "../styles/card.scss";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
+const Card = ({ fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-const Card = ({fetchUrl}) => {
-      const [movies, setMovies] = useState([]);
-      const [trailerUrl, setTrailerUrl] = useState("");
-    
-      useEffect(() => {
-        async function fetchData() {
-          const request = await axios.get(fetchUrl);
-          setMovies(request.data.results);
-          return request;
-        }
-        fetchData();
-      }, [fetchUrl]);
-    
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-
-    return (
-        <div className="card" >
+  return (
+    <div class="card">
+    <div class="card__wrapper">
         {movies.map((ani) => {
-            return (
-                <div className="card__data">
-  
-                  {" "}
-                  <img
-                    className="card__img"
-                    src={`https://image.tmdb.org/t/p/original/${ani.backdrop_path}`}
-                    alt={ani.title}
-                  />
-            
+          return (
+            <div className="card__data">
+              <div className="item">
+                {" "}
+                <img
+                  className="card__img"
+                  src={`https://image.tmdb.org/t/p/original/${ani.backdrop_path}`}
+                  alt={ani.title}
+                />
               </div>
-            )
-        }  )}
-            
-        </div>
-    )
-}
+            </div>
+            // <div class="tile">
+            //   <div class="tile__media">
+            //     <img
+            //       class="tile__img"
+            //       src={`https://image.tmdb.org/t/p/original/${ani.backdrop_path}`}
+            //       alt=""
+            //     />
+            //   </div>
+            // </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
